c8fe0dfd2cc93b653d133c995bec5d9014976eb5
Myles <mazehko@alphaworks.tech>
2022-03-22 10:55:52 -0700

    Update App.js

--- App.js
+++ App.js
@@ -7,28 +7,21 @@ export default () => {
     new Animated.Value(0)
     ).current;
 
-    useEffect(() => {
-      Animated.timing(translation, {
-        toValue: 50,
-        useNativeDriver: true,
-      }).start();
-    }, []);
+    useEffect(() => { 
+      Animated.timing(translation, { 
+        toValue: 50, useNativeDriver: true,
+      }).start(); }, []); 
 
 
   return (
+
     <Animated.View
       style={{
         width: 100,
         height: 100,
         backgroundColor: 'red',
-        transform: [{translateX: translation}],
+        transform: [{translateX: translation}]
       }}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      />
+  )
+    };
