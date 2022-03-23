9ef7c96b18647338bccfd5a0e8b4bead3f10276a
Myles <mazehko@alphaworks.tech>
2022-03-23 10:29:17 -0700

    Update App.js

--- App.js
+++ App.js
@@ -37,6 +37,34 @@ export default () => {
         width: 100,
         height: 100,
         backgroundColor: '#B76E79',
+        marginBottom: 10,
+        opacity: firstOpacity,
+        transform: [
+          {translateX: translation.x},
+          {translateY: translation.y},
+        ],
+      }}
+      />
+      <Animated.View
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: '#B76E79',
+        marginBottom: 10,
+        opacity: secondOpacity,
+        transform: [
+          {translateX: translation.x},
+          {translateY: translation.y},
+        ],
+      }}
+      />
+      <Animated.View
+      style={{
+        width: 100,
+        height: 100,
+        backgroundColor: '#B76E79',
+        marginBottom: 10,
+        opacity: thirdOpacity,
         transform: [
           {translateX: translation.x},
           {translateY: translation.y},
