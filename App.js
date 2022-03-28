033a804c7d21d93899718c37a6f4420d250462df
Myles <mazehko@alphaworks.tech>
2022-03-28 10:02:52 -0700

    Update App.js
    Move animation 100 pixels to the left

--- App.js
+++ App.js
@@ -2,34 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Animated, Easing } from 'react-native';
 import { useEffect, useRef } from 'react';
 
-export default () => {
-  const firstOpacity = useRef(
-    new Animated.Value(0)
-    ).current;
-
-    const secondOpacity = useRef(
-      new Animated.Value(0)
-      ).current;
-
-      const thirdOpacity = useRef(
-        new Animated.Value(0)
-        ).current;  
+export default function App() => {
+  const translation = useRef(new Animated.Value(0)).
+  current;
 
     useEffect(() => { 
-      Animated.stagger(100, [
-        Animated.timing(firstOpacity, {
-          toValue: 1,
+        Animated.timing(translation, {
+          toValue: 100,
+          duration: 1000,
           useNativeDriver: true,
         }),
-        Animated.timing(secondOpacity, {
-          toValue: 1,
-          useNativeDriver: true,
-        }),
-        Animated.timing(thirdOpacity, {
-          toValue: 1,
-          useNativeDriver: true,
-        }),
-      ]).start();
     });  
      
 
@@ -40,37 +22,8 @@ export default () => {
         width: 100,
         height: 100,
         backgroundColor: '#B76E79',
-        marginBottom: 10,
-        opacity: firstOpacity,
         transform: [
-          {translateX: translation.x},
-          {translateY: translation.y},
-        ],
-      }}
-      />
-      <Animated.View
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: '#B76E79',
-        marginBottom: 10,
-        opacity: secondOpacity,
-        transform: [
-          {translateX: translation.x},
-          {translateY: translation.y},
-        ],
-      }}
-      />
-      <Animated.View
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: '#B76E79',
-        marginBottom: 10,
-        opacity: thirdOpacity,
-        transform: [
-          {translateX: translation.x},
-          {translateY: translation.y},
+          {translateX: translation},
         ],
       }}
       />
